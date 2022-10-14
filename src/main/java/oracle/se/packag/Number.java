4d69882e1b307cb11044ce9cd08d03b2e507758e
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-14 21:18:07 +0900

    package and internal java package

---
+++ src/main/java/oracle/se/packag/Number.java
@@ -0,0 +1,30 @@
+package oracle.se.packag;
+
+/*import java.lang.Number;
+*
+* В СЛУЧАЕ УКАЗАНИЯ ДАННОГО ИМПОРТА ВОЗНИКНЕТ ОШИБКА КОМПИЛЯЦИИ
+*  ТАК КАК JAVA НЕ СМОЖЕТ ПОНЯТЬ ЭКЗЕМПЛЯР КАКОГО КЛАССА ИСПОЛЬЗОВАТЬ
+* */
+
+public class Number {
+
+    Number number;
+    java.lang.Number numberj;
+
+    Number(java.lang.Number numberj) {
+        this.numberj = numberj;
+        System.out.println("Создание oracle.se.packag.Number с помощью java.lang.Number");
+    }
+
+    Number(Number number) {
+        this.number = number;
+        System.out.println("Создание oracle.se.packag.Number с помощью oracle.se.packag.Number");
+    }
+
+    public static void main(String[] args) {
+        /* Создаеться 2 экземрляра класса oracle.se.packag.Number*/
+        Number n = new Number(10);
+        Number n2 = new Number(n);
+
+    }
+}
