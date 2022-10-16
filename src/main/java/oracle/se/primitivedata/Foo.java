2e25727b582f91cad9d3b34a96d80f58d7cc6f32
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-17 01:24:15 +0900

    primitive variable

---
+++ src/main/java/oracle/se/primitivedata/Foo.java
@@ -0,0 +1,18 @@
+package oracle.se.primitivedata;
+
+public class Foo {
+
+    public static void main(String[] args) {
+/*Демонстрация объявления переменных. Переменные разных типов так же могут записаны в одной строке (Ctrl + O
+* разделит их на разные строки) */
+        byte b;
+        short s;
+        int i;
+        long firstL, secondL;
+        float f;
+        double d;
+        boolean firstBl, secondBl;
+        char c;
+        int myInt; char myChar;
+    }
+}
