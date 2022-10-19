22f6283362cbf5ab8d61e77d4ed0dd0fa34a6823
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-20 00:03:41 +0900

    string splice and dice

---
+++ src/main/java/oracle/se/primitivedata/strings/StringSpliceAndDice.java
@@ -0,0 +1,21 @@
+package oracle.se.primitivedata.strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringSpliceAndDice {
+
+    public static void main(String[] args) {
+        String[] wordArray = new String[]{"Hello", "word", "How", "are", "you"};
+        List<String> words = new ArrayList<>(Arrays.asList(wordArray));
+        String sentence = "";
+        for (var s : words) {
+            sentence += s + " ";
+        }
+        System.out.println(sentence);
+
+        sentence = String.join(" ", wordArray );
+        System.out.println(sentence);
+    }
+}
