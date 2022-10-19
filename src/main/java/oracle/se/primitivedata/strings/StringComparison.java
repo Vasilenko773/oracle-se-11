c69ea270c7ffa4d19b2a322fc42a8106d724502e
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-19 23:19:45 +0900

    string comparison

---
+++ src/main/java/oracle/se/primitivedata/strings/StringComparison.java
@@ -0,0 +1,15 @@
+package oracle.se.primitivedata.strings;
+
+public class StringComparison {
+
+    public static void main(String[] args) {
+
+        String nullString = null;
+        String hello = "Hello";
+        String hello1 = "hello";
+        String hello2 = new String(new char[] {'H', 'e', 'l', 'l', 'o'});
+        System.out.println(hello.equals(hello2));
+        /* Сравнивать с null можно только если null будет справа иначе возникнет исключение NullPointerException*/
+//       nullString.equals(hello2);
+    }
+}
