52525f36e445e67c1eeadc5c683de1bec7d648e1
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-20 23:55:58 +0900

    stringBuilder getChars

--- src/main/java/oracle/se/primitivedata/stringbuilder/StringBuilderExtras.java
+++ src/main/java/oracle/se/primitivedata/stringbuilder/StringBuilderExtras.java
@@ -4,7 +4,7 @@ public class StringBuilderExtras {
 
     public static void main(String[] args) {
         StringBuilder builder = new StringBuilder();
-        builder.append("   ACDC        ");
+        builder.append("ACDC        ");
         System.out.println(builder + "*");
         System.out.println(builder.length());
         System.out.println(builder.capacity());
@@ -18,5 +18,9 @@ public class StringBuilderExtras {
         System.out.println(builder + "*");
         System.out.println(builder.length());
         System.out.println(builder.capacity());
+        StringBuilder builder1 = new StringBuilder("sasdasfwe");
+        char[] myCharArray = new char[5];
+        builder1.getChars(0, 5, myCharArray, 0);
+        System.out.println(myCharArray);
     }
 }
