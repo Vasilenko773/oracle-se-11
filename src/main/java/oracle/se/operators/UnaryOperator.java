ad027ebd260eaa154657c107f7e75779a287c109
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-21 02:16:23 +0900

    unary operator

---
+++ src/main/java/oracle/se/operators/UnaryOperator.java
@@ -0,0 +1,12 @@
+package oracle.se.operators;
+
+public class UnaryOperator {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int a1 = a++;
+        System.out.println(a); //2
+        System.out.println(a1); //1
+
+    }
+}
