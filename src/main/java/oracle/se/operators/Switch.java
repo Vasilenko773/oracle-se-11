5ff051c1bbbeee7465fbde55e812ac0a6afa668d
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-21 19:55:11 +0900

    switch-case operator

---
+++ src/main/java/oracle/se/operators/Switch.java
@@ -0,0 +1,24 @@
+package oracle.se.operators;
+
+import java.util.List;
+
+public class Switch {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("A", "B", "C", "D");
+        int iteration = 0;
+        for (var s : strings) {
+            System.out.println(++iteration + " " + s);
+            switch (s) {
+                case "A":
+                    System.out.println("DO A");
+                default:
+                    System.out.println("THIS");
+                case "B":
+                    System.out.println("DO B");
+                case "C":
+                    System.out.println("DO C");
+            }
+        }
+    }
+}
