b3e8acdeb8052d18434fe09eac114d4897bd0bad
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-26 00:03:41 +0900

    initialization order more

---
+++ src/main/java/oracle/se/object/ForwardReference.java
@@ -0,0 +1,32 @@
+package oracle.se.object;
+
+public class ForwardReference {
+    public static void main(String[] args) {
+        Thing one = new Thing();
+        System.out.println(one.toString());
+    }
+
+    static class Thing {
+        Thing() {
+            secondString = "b" + thirdString;
+        }
+
+        {
+            firstString = "a" + thirdString;
+        }
+
+        static {
+            thirdString = "c";
+        }
+
+        String firstString;
+        String secondString;
+        static String thirdString;
+
+        @Override
+        public String toString() {
+            return firstString + secondString + thirdString;
+        }
+
+    }
+}
