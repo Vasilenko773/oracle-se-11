1b1d46eb43f0bad5d18d122bf9a4d8cea6bb9886
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-10-27 01:27:38 +0900

    constructor classes

---
+++ src/main/java/oracle/se/methodandconstructor/ConstructorExample.java
@@ -0,0 +1,32 @@
+package oracle.se.methodandconstructor;
+
+import lombok.Getter;
+
+class ATest {
+    ATest() {
+        System.out.println("I an constructor ATest");
+    }
+}
+
+@Getter
+class BTest extends ATest {
+    private String name;
+
+    BTest() {
+    }
+
+    BTest(String name) {
+        this.name = name;
+    }
+}
+
+public class ConstructorExample {
+
+    public static void main(String[] args) {
+        BTest bTest = new BTest("first");
+        System.out.println(bTest.getName());
+        System.out.println("========");
+        BTest bTest1 = new BTest();
+        System.out.println(bTest1);
+    }
+}
