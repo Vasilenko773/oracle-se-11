8f19109d00224e61f927a910351eee691947a832
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-11-01 01:46:27 +0900

    abstract class

---
+++ src/main/java/oracle/se/extending/Auto.java
@@ -0,0 +1,31 @@
+package oracle.se.extending;
+
+public class Auto extends Vehicle {
+
+    public Auto(VehicleType type, String owner, String make) {
+        super(type, owner, make);
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("drive auto");
+    }
+
+    @Override
+    public void park() {
+        System.out.println("park auto");
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("make noise auto");
+    }
+
+    public static void main(String[] args) {
+        Auto car = new Auto(VehicleType.Automobile, "Steeven", "Mazda");
+        System.out.println(car);
+        car.drive();
+        car.park();
+        car.makeNoise();
+    }
+}
---
+++ src/main/java/oracle/se/extending/Vehicle.java
@@ -0,0 +1,29 @@
+package oracle.se.extending;
+
+import lombok.ToString;
+
+@ToString
+public abstract class Vehicle {
+
+    protected enum VehicleType {
+        Automobile, Motorcycle, Moped, Bicycle, Scooter
+    }
+
+    private VehicleType type;
+    private String owner;
+    private String make;
+
+    public Vehicle(VehicleType type, String owner, String make) {
+        this.type = type;
+        this.owner = owner;
+        this.make = make;
+    }
+
+    public abstract void drive();
+    public abstract void park();
+    public abstract void makeNoise();
+
+    public void set() {
+        System.out.println("sasdasd");
+    }
+}
