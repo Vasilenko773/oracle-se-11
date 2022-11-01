e614f567a2df6857bf8cbffbde1b568cd6879408
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-11-01 12:51:43 +0900

    abstract class example

---
+++ src/main/java/oracle/se/extending/AbstractClassExample.java
@@ -0,0 +1,19 @@
+package oracle.se.extending;
+
+public abstract class AbstractClassExample {
+
+    public static final String ABSTRACT_CONSTANT = "Abstract";
+
+    public static final void doThisOnlyThis() {
+        System.out.println("static final method ");
+    }
+
+    public final void doThisAndThat() {
+        System.out.println("my final method");
+        System.out.println(doThis() + " " + doThat());
+    }
+
+    abstract String doThis();
+
+    abstract String doThat();
+}
