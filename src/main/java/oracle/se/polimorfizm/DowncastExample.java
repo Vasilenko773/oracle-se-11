f58ee1464779d292d77d7795b0c1b7f67d6b6f1a
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-11-02 22:50:54 +0900

    up casting in polimorfizm

--- src/main/java/oracle/se/polimorfizm/DowncastExample.java
+++ src/main/java/oracle/se/polimorfizm/DowncastExample.java
@@ -9,24 +9,31 @@ class Animal {
 class Dog extends Animal {
 
     public void printDog() {
-        System.out.println("dog") ;
+        System.out.println("dog");
+    }
+
+    public String getDogString() {
+        return "dog";
     }
 }
 
 class Cat extends Animal {
     public void printCat() {
-        System.out.println("cat") ;
+        System.out.println("cat");
+    }
+
+    public String getCatString() {
+        return "cat";
     }
 }
-
 
 
 public class DowncastExample {
 
     public static void main(String[] args) {
         DowncastExample dwex = new DowncastExample();
-        Animal dog = new Dog();
-        Animal cat = new Cat();
+        Animal genericDog = new Dog();
+        Animal genericCat = new Cat();
 
         Dog pug = new Dog();
         Cat specificCat = new Cat();
@@ -35,8 +42,23 @@ public class DowncastExample {
         dwex.testCat(specificCat);
         dwex.testAnimal(pug);
         dwex.testAnimal(specificCat);
-        dwex.testAnimal(cat);
-        dwex.testAnimal(dog);
+        dwex.testAnimal(genericCat);
+        dwex.testAnimal(genericDog);
+
+        if (((Dog) genericDog).getDogString().equals("dog")) {
+            System.out.println("yep, parent casting from child class");
+        }
+
+        Cat c = (Cat) dwex.passThrough(genericCat);
+        c.getCatString();
+        c.printCat();
+        /*
+        dwex.testDog((Dog) genericCat);
+        dwex.testCat((Cat) genericDog);
+        dwex.testAnimal((Cat) genericDog);
+        dwex.testAnimal((Dog) genericCat);
+        При енправильном присвоениитипов будет ошибка компеляции "cannot be cast to class"
+         */
     }
 
     public void testAnimal(Cat cat) {
@@ -54,12 +76,15 @@ public class DowncastExample {
         animal.printAnimal();
     }
 
-
     public void testDog(Dog dog) {
         dog.printDog();
     }
 
     public void testCat(Cat cat) {
         cat.printCat();
+    }
+
+    public Object passThrough(Object o) {
+        return o;
     }
 }
