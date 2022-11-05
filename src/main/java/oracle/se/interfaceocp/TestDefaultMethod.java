991f2102e17039735a6adad19382f06d1aad6ce4
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-11-06 00:29:10 +0900

    casting interface and overload defaulth methods

---
+++ src/main/java/oracle/se/interfaceocp/TestDefaultMethod.java
@@ -0,0 +1,40 @@
+package oracle.se.interfaceocp;
+
+interface Teachable {
+
+    default void teachTheseLessons() {
+        System.out.println("default method");
+    }
+
+    void teach();
+}
+
+interface Trainable {
+
+    default void teachTheseLessons() {
+        System.out.println("default method trainable");
+    }
+
+    void train();
+}
+
+public class TestDefaultMethod implements Teachable, Trainable {
+    @Override
+    public void teach() {
+        teachTheseLessons();
+    }
+
+    public void train() {
+        System.out.println("train method testdefault");
+    }
+
+    public void teachTheseLessons() {
+        System.out.println("Переопределил метол");
+    }
+
+    public static void main(String[] args) {
+        TestDefaultMethod t = new TestDefaultMethod();
+        t.teach();
+        t.train();
+    }
+}
