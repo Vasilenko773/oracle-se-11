908515eeddb0a8e704e440f4ed35879c74ba94dd
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-11-08 00:37:28 +0900

    anonymous class

---
+++ src/main/java/oracle/se/lambda/AnonymousClass.java
@@ -0,0 +1,55 @@
+package oracle.se.lambda;
+
+import java.util.Comparator;
+
+public class AnonymousClass {
+
+    interface Helloable {
+        void hello();
+    }
+
+    public static void main(String[] args) {
+        AnonymousClass a = new AnonymousClass();
+
+        class LocalClass extends Object {
+            public void hello() {
+                System.out.println("hello local class");
+            }
+        }
+
+        new LocalClass().hello();
+        Object anonymClass = new Object() {
+            public String toString() {
+                return "Hello anonymous class";
+            }
+        };
+
+        System.out.println(anonymClass.toString());
+
+        Helloable anonymInterface = new Helloable() {
+            @Override
+            public void hello() {
+                System.out.println("Hello anonymous class implements " +
+                        "interface");
+            }
+        };
+        anonymInterface.hello();
+
+        //ПЕРЕДАЧА АНОНИМНОГО КЛАССА В ПАРАМЕТЫР МЕТОДВ
+        a.printHello(new Helloable() {
+            @Override
+            public void hello() {
+                System.out.println("Hello anonymous class implements " +
+                        "interface");
+            }
+        });
+
+        //ИСПОЛЬЗОВАНИЕ ДЯМБДА ВЫРАЖЕНИЯ
+        a.printHello(() -> System.out.println("Hello lambda"));
+
+    }
+
+    public void printHello(Helloable h) {
+        h.hello();
+    }
+}
