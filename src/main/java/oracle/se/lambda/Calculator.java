56f74b7bc12beb50e679279312381c7cc267928a
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2022-11-08 20:13:33 +0900

    lambda expression

---
+++ src/main/java/oracle/se/lambda/Calculator.java
@@ -0,0 +1,29 @@
+package oracle.se.lambda;
+
+public class Calculator {
+
+    interface UnaryIntegerOperation {
+        int calculate(int a);
+    }
+
+    public int calculate(int a, UnaryIntegerOperation op) {
+        return op.calculate(a);
+    }
+
+    public static void main(String[] args) {
+        Calculator myApp = new Calculator();
+        UnaryIntegerOperation square = (int a) -> a * a;
+        UnaryIntegerOperation cube = a -> a * a * a;
+        UnaryIntegerOperation increment = (a) -> a + 1;
+        UnaryIntegerOperation decrement = a -> {
+            return a - 1;
+        };
+
+        int value = 2;
+        System.out.println(myApp.calculate(value, square));
+        System.out.println(myApp.calculate(value, cube));
+        System.out.println(myApp.calculate(value, increment));
+        System.out.println(myApp.calculate(value, decrement));
+
+    }
+}
